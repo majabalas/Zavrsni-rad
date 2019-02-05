a5b6b1f0d4bf6ef92c522171f477c47372fb5645
Maja Balas <44590513+majabalas@users.noreply.github.com>
2019-02-06 00:41:03 +0100

    Add files via upload

---
+++ rezerviraj_eventLogin/app/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "url"=>"http://polaznik06.edunova.hr/rezerviraj_eventLogin/",
+    "dev"=>true,
+    "nazivApp" => "Rezerviraj Event!",
+    "db" => [
+        "host" => "localhost",
+        "name" => "hefest_mbzavrsnirad",
+        "user" => "hefest_mbalas",
+        "password" => "mBalas123!"
+    ]
+];
